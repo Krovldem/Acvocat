008cbffc78ba8e2248cec6fcf5d979fd3b28026e
Ivan Lukovkin <121473397+iamlukovkin@users.noreply.github.com>
2024-08-03 15:07:48 +0300

    Creating user model

--- src/main/python/ru/krovlDem/acvocat/acvocat/settings.py
+++ src/main/python/ru/krovlDem/acvocat/acvocat/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ru.krovlDem.users',
-    'ru.krovlDem.pages'
 ]
 
 MIDDLEWARE = [
--- src/main/python/ru/krovlDem/users/models.py
+++ src/main/python/ru/krovlDem/users/models.py
@@ -1,3 +1,45 @@
-from django.db import models
+import re
 
-# Create your models here.
+from django.db import models
+from ru.iamlukovkin.models import User as UserModel
+
+
+class User(models.Model):
+
+    _email: str = models.CharField(max_length=100, unique=True, db_column="email")
+
+    @property
+    def email(self) -> str: return self._email
+
+    @email.setter
+    def email(self, email: str) -> None:
+        if not UserModel.email_is_valid(email):
+            return
+        self._email = email
+
+    _full_name: str = models.CharField(max_length=100, db_column="full_name")
+
+    @property
+    def full_name(self) -> str:
+        return self._full_name
+
+    @full_name.setter
+    def full_name(self, full_name: str) -> None:
+        if not self.full_name_is_valid(full_name):
+            return
+        self._full_name = full_name
+
+    def __init__(self, full_name: str, email: str):
+        super().__init__()
+        self.full_name = full_name
+        self.email = email
+
+    @classmethod
+    def email_is_valid(cls, email: str) -> bool:
+        return bool(re.search(r'[\w.-]+@[\w.-]+.\w+', email))
+
+    @classmethod
+    def full_name_is_valid(cls, full_name: str) -> bool:
+        if len(full_name) < 2:
+            return False
+        return True
